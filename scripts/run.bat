6249ba86fed0994bb0d897e4d3534d0871007b35
Jesse Pavel <jpavel@alum.mit.edu>
2018-09-20 04:52:44 -0400

    Dist artifact

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,5 +1,1 @@
-@echo off
-pushd %~dp0..
-java -Xmx256M -cp build\production\Treehouse;lib\* -splash:assets/images/Treehouse-Splash.png ^
-    com.worldofshots.treehouse.Treehouse
-popd
+@java -jar LetterGame.jar
